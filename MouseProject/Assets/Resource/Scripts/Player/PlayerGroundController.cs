21c053b7c3cd2b65de0a0b6d7ee99c62b925dd36
miguelmplpadilla <89489412+miguelmplpadilla@users.noreply.github.com>
2023-03-13 19:04:06 +0100

    Arreglos mergeo

--- MouseProject/Assets/Resource/Scripts/Player/PlayerGroundController.cs
+++ MouseProject/Assets/Resource/Scripts/Player/PlayerGroundController.cs
@@ -9,47 +9,17 @@ public class PlayerGroundController : MonoBehaviour
 
     public bool isEnemy;
 
-    public GameObject parentGO;
-    public EnemyMovement enemyScript;
-    public PlayerMovement playerScript;
-
 
     private Animator animator;
 
     private void Awake()
     {
         animator = GetComponentInParent<Animator>();
-
-        if (isEnemy == true)
-        {
-            enemyScript = parentGO.GetComponent<EnemyMovement>();
-        }
-        else
-        {
-            playerScript = parentGO.GetComponent<PlayerMovement>();
-        }
-
     }
 
     private void Update()
     {
         animator.SetBool("grounded", isGrounded);
-    }
-
-
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.CompareTag("Ground"))
-        {
-            if (isEnemy == true)
-            {
-                //enemyScript.speed = 1;
-            }
-            else
-            {
-                //playerScript.speed = 1;
-            }
-        }
     }
 
     private void OnTriggerStay2D(Collider2D other)
@@ -59,9 +29,7 @@ public class PlayerGroundController : MonoBehaviour
             isGrounded = true;
         }
     }
-
-
-
+    
     private void OnTriggerExit2D(Collider2D other)
     {
         if (other.CompareTag("Ground"))
